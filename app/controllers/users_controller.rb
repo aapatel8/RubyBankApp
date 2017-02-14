fb46717ab034ee287ae84d3906bcfae88f3c7fce
Zachery DeLong <zacherypd@gmail.com>
2017-02-14 14:02:46 -0500

    Fixing user lookup... oops.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,7 +16,7 @@ class UsersController < ApplicationController
   # GET /users/1
   # GET /users/1.json
   def show
-      @user = User.find("1")
+      @user = User.find( params[:id] )
   end
 
   # GET /users/new
