fa8a62e723bae3a80feadaa9b5a038a93bb4b469
Zachery DeLong <zacherypd@gmail.com>
2017-02-21 14:52:49 -0500

    Admin dashboard shows pending xacts.
    Signed-off-by: Zachery DeLong <zacherypd@gmail.com>

--- app/controllers/admin_dashboards_controller.rb
+++ app/controllers/admin_dashboards_controller.rb
@@ -2,5 +2,10 @@ class AdminDashboardsController < ApplicationController
     before_action :authenticate_admin!
 
     def show
+        @pendingXacts = Transaction.where(status: "pending")
+
+        if @pendingXacts == nil
+            @pendingXacts = []
+        end
     end
 end
