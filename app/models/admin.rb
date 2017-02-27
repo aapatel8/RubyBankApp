35353a948c95492ea2e440ccdf140c0acacde09d
Zachery DeLong <zacherypd@gmail.com>
2017-02-27 17:37:04 -0500

    Adding method to retrieve default admin
    Used to prevent deletion.
    
    Signed-off-by: Zachery DeLong <zacherypd@gmail.com>

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -3,4 +3,10 @@ class Admin < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, 
          :recoverable, :rememberable, :trackable, :validatable
+
+  def self.GetDefaultAdmin
+    @invincibleAdmin = Admin.all().order('created_at').limit(1)
+
+    @invincibleAdmin = @invincibleAdmin[0]
+  end
 end
