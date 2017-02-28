4cf64d01daf64ea0dd455017fc07bd8aeac27f38
Zachery DeLong <zacherypd@gmail.com>
2017-02-28 18:29:26 -0500

    Adjusting admin UI to use updated transactions
    Signed-off-by: Zachery DeLong <zacherypd@gmail.com>

--- app/controllers/admin_dashboards_controller.rb
+++ app/controllers/admin_dashboards_controller.rb
@@ -2,10 +2,11 @@ class AdminDashboardsController < ApplicationController
     before_action :authenticate_admin!
 
     def show
-        @pendingXacts = Transaction.where(status: "pending")
+        @pendingXacts = Transaction.GetPending
 
         if @pendingXacts == nil
             @pendingXacts = []
         end
     end
+
 end
