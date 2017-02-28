ef604f0051c054bfb598092e631178d1ac0a8498
Zachery DeLong <zacherypd@gmail.com>
2017-02-28 18:29:04 -0500

    Adding methods to approve/reject transactions
    Signed-off-by: Zachery DeLong <zacherypd@gmail.com>

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,4 +1,16 @@
 class Transaction < ApplicationRecord
   belongs_to :source_account, :class_name => 'Account', optional: true
   belongs_to :dest_account, :class_name => 'Account', optional: true
+
+    def self.GetPending
+        Transaction.where(status: "pending")
+    end
+
+    def self.ApproveTransaction(transaction)
+        transaction.status = "approved"
+    end
+
+    def self.RejectTransaction(transaction)
+        transaction.status = "rejected"
+    end
 end
