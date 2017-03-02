c960320ca6b5d307be17c90343412f51754187b7
Zachery DeLong <zacherypd@gmail.com>
2017-03-02 09:41:34 -0500

    Creating users as an admin now working.
    Signed-off-by: Zachery DeLong <zacherypd@gmail.com>

--- app/controllers/admin_users_controller.rb
+++ app/controllers/admin_users_controller.rb
@@ -13,8 +13,8 @@ class AdminUsersController < ApplicationController
 
     respond_to do |format|
       if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
-        format.json { render :show, status: :created, location: @user }
+        format.html { redirect_to admin_user_path(@user), notice: 'User was successfully created.' }
+        format.json { render :show, status: :created, location: admin_user_path(@user) }
       else
         format.html { render :new }
         format.json { render json: @user.errors, status: :unprocessable_entity }
@@ -33,7 +33,7 @@ class AdminUsersController < ApplicationController
   def update
     @user = User.find(params[:id])
     respond_to do |format|
-      if @user.update(user_params)
+      if @user.update(update_params)
         format.html { redirect_to admin_user_path(@user), notice: 'User was successfully updated.' }
         format.json { render :show, status: :ok, location: admin_user_path(@user) }
       else
@@ -54,6 +54,10 @@ class AdminUsersController < ApplicationController
 
   private
   def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
+  def update_params
     params.require(:user).permit(:name, :email)
   end
 end
