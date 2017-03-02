a02e65cd34fcc2debc247b9dc2cf90b19c7c458d
Kai Presler-Marshall <kpresle@ncsu.edu>
2017-03-02 18:52:10 -0500

    admins can edit account

--- app/controllers/admin_dashboards_controller.rb
+++ app/controllers/admin_dashboards_controller.rb
@@ -1,7 +1,13 @@
 class AdminDashboardsController < ApplicationController
     before_action :authenticate_admin!
 
+
     def show
+
+        @current_user = current_admin
+        @uid = @current_user.id
+
+
         @pendingXacts = Transaction.GetPendingAdminApproval
 
         if @pendingXacts == nil
